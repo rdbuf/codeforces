eddd66884dcef4e16a77618be868b670220cd288
Ilya Pikulin <ilya.pikulin@gmail.com>
2024-11-04 00:53:00 +0300

    update machinery

--- build-and-run.sh
+++ build-and-run.sh
@@ -9,13 +9,14 @@ clang++ -Wall -Wextra -Wconversion -g -fsanitize=undefined -fsanitize=address -f
 	-Wno-sign-conversion -Wno-sign-compare -Werror=shadow -Werror=return-type -std=c++20 ${file} -o ${bin_file}
 chmod +x ${bin_file}
 
-mkdir -p /tmp/${file}
-touch /tmp/${file}/test.in
-touch /tmp/${file}/test.out_expected
-touch /tmp/${file}/test.out_actual
+mkdir -p ${file}.output
+test_in=${file}.output/test.in
+out_expected=${file}.output/test.out_expected
+out_actual=${file}.output/test.out_actual
 
-awk '/входные данныеСкопировать/{flag=1; next}/выходные данныеСкопировать/{flag=0} flag' $file > /tmp/${file}/test.in
-awk '/выходные данныеСкопировать/{flag=1; next}/\*\*\* TEST CASES END \*\*\*\*\//{flag=0} flag' $file > /tmp/${file}/test.out_expected
-${bin_file} < /tmp/${file}/test.in > /tmp/${file}/test.out_actual
-sdiff <(nl /tmp/${file}/test.out_expected) <(nl /tmp/${file}/test.out_actual)
+awk '/входные данныеСкопировать/{flag=1; next}/выходные данныеСкопировать/{flag=0} flag' $file > $test_in
+awk '/выходные данныеСкопировать/{flag=1; next}/\*\*\* TEST CASES END \*\*\*\*\//{flag=0} flag' $file > $out_expected
+${bin_file} < $test_in > $out_actual
+printf " %.0s" {1..8}; printf "EXPECTED"; printf " %.0s" {1..56}; printf "ACTUAL\n"
+sdiff <(nl $out_expected) <(nl $out_actual)
 if [[ ${PIPESTATUS[0]} -eq 0 ]]; then echo PASSED; else echo MISMATCH; fi
